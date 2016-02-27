4e0b8ab469e8d017cf0e3406a43fad2d4125958b
Nishad Mathur <nishadmathur@gmail.com>
2016-02-27 14:48:35 +0000

    Created a skeleton game view.

--- prototype/game/urls.py
+++ prototype/game/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import url
 from game import views
 
 urlpatterns = [
-    url(r'^$', views.Index.as_view(), name='game-index'),
+    url(r'^$', views.Index.as_view(), name='index'),
     url(r'^new_game/?$', views.NewGame.as_view(), name='new_game'),
     url(r'^top_table/?$', views.HighScoreTable.as_view(), name='high_score_table'),
+    url(r'^games/(?P<game_id>\d+)/?', views.InProgressGame.as_view(), name='in-progress_games')
 ]
