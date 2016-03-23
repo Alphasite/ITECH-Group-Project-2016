f10ccaa9d35478a068d3927fb49588c1c375177f
Nishad Mathur <nishadmathur@gmail.com>
2016-03-23 14:19:54 +0000

    Fixed Styling.

--- prototype/game/themes/Zombuy.py
+++ prototype/game/themes/Zombuy.py
@@ -37,32 +37,32 @@ class Simulation:
     def items(self):
         return [
             Item(
-                "BRAINZ", 25, 100, 0.8, SalesCalculation(3, 200),
+                "BRAINZ", 25, 100, 15, SalesCalculation(3, 200),
                 description="Its not just a brain; its a farm reared, ginger haired M&S brain."
             ),
             Item(
-                "Genuine Fake Chinese Brainz", 4, 500, 0.2, SalesCalculation(3, 1000, -500),
+                "Genuine Fake Chinese Brainz", 4, 500, 50, SalesCalculation(3, 1000, -500),
                 description="º¤ø,¸¸,ø¤ºº¤ø,¸¸,¸,øºø,¸,ø¤ºº¤ø,¸¸,ø¤HIGHLY RECOMMENDED º."
             ),
             Item(
-                "ARMZZZ", 8, 20, 0.2, SalesCalculation(5, 24, -4),
+                "ARMZZZ", 8, 20, 2.0, SalesCalculation(5, 24, -4),
                 description="For when your own just wont stay on..."
             ),
             Item(
-                "Stapler", 20, 10, 0.1, SalesCalculation(5, 12, -2),
+                "Stapler", 20, 10, 1.4, SalesCalculation(5, 12, -2),
                 description="See 'ARMZZZ'."
             ),
             Item(
-                "Dr Zomboids Zomball Cure!", 50, 20, 0.2, SalesCalculation(6, 20, 10),
+                "Dr Zomboids Zomball Cure!", 50, 20, 4, SalesCalculation(6, 20, 10),
                 description="Got a case of the humans? Make it disappear with just this one easy pill!"
             ),
             Item(
-                "Estus Flask", 100, 1, 0.8, SalesCalculation(10, 2),
+                "Estus Flask", 100, 1, 24, SalesCalculation(10, 2),
                 description="A green glass bottle of unknown make. But that is of little concern, "
                             "for any Undead knows the value of these precious flasks."
             ),
             Item(
-                "Dr Zomboids Stay Upper (10 pack)", 5, 100, 0.1, SalesCalculation(2, 50),
+                "Dr Zomboids Stay Upper (10 pack)", 5, 100, 6, SalesCalculation(2, 50),
                 description="The little blue pill who could..."
             ),
             ]
